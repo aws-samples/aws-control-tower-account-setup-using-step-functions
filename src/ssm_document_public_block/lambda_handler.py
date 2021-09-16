5f4df8a361955978ce1e60d5f3ddd3fdde69ffd0
Justin Plock <jplock@amazon.com>
2021-09-15 20:41:28 -0400

    WIP

---
+++ src/ssm_document_public_block/lambda_handler.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+* SPDX-License-Identifier: MIT-0
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this
+* software and associated documentation files (the "Software"), to deal in the Software
+* without restriction, including without limitation the rights to use, copy, modify,
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+from concurrent.futures import ThreadPoolExecutor
+import os
+from typing import Dict, Any
+
+from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.utilities.typing import LambdaContext
+import boto3
+
+EXECUTION_ROLE_NAME = os.environ["EXECUTION_ROLE_NAME"]
+tracer = Tracer()
+logger = Logger()
+
+
+@tracer.capture_method
+def schedule_ssm_document_public_block(
+    account_id: str, region: str, credentials: Dict[str, str]
+) -> None:
+    session = boto3.Session(
+        aws_access_key_id=credentials["AccessKeyId"],
+        aws_secret_access_key=credentials["SecretAccessKey"],
+        aws_session_token=credentials["SessionToken"],
+    )
+    ssm = session.client("ssm", region_name=region)
+
+    setting_id = f"arn:aws:ssm:{region}:{account_id}:servicesetting/ssm/documents/console/public-sharing-permission"
+    ssm.update_service_setting(SettingId=setting_id, SettingValue="Disable")
+
+
+@tracer.capture_lambda_handler
+@logger.inject_lambda_context(log_event=True)
+def handler(event: Dict[str, Any], context: LambdaContext) -> None:
+
+    account_id = event.get("account", {}).get("accountId")
+    if not account_id:
+        raise Exception("Account ID not found in event")
+
+    session = boto3.Session()
+    ec2 = session.client("ec2")
+
+    all_regions = [
+        region["RegionName"]
+        for region in ec2.describe_regions(
+            Filters=[{"Name": "opt-in-status", "Values": ["opt-in-not-required"]}],
+            AllRegions=False,
+        )["Regions"]
+    ]
+
+    sts = session.client("sts")
+
+    role_arn = f"arn:aws:iam::{account_id}:role/{EXECUTION_ROLE_NAME}"
+    credentials = sts.assume_role(
+        RoleArn=role_arn, RoleSessionName="ssm_document_public_block"
+    )["Credentials"]
+
+    args = ((account_id, region, credentials) for region in all_regions)
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for _ in executor.map(lambda f: schedule_ssm_document_public_block(*f), args):
+            pass
